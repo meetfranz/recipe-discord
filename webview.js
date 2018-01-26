d86ebe742e407144dca82332e69b2b567794d118
Daniel Smith <StormPooper@users.noreply.github.com>
2018-01-26 21:48:04 +0000

    Get counts for both direct and indirect badges
    Annoyingly the direct count badge disappears from the currently selected guild icon, so it has to be read from the channel list in that case.
    
    The indirect count will just total up the number of unread flags visible in the current context.

--- webview.js
+++ webview.js
@@ -1,17 +1,38 @@
 import path from 'path';
 
-module.exports = (Franz) => {
+module.exports = Franz => {
   const getMessages = function getMessages() {
-    // get unread messages
-    const count = document.querySelectorAll('.guilds-wrapper .badge').length;
+    // get direct messages
+    let direct = 0;
+    const guildDirect = document.querySelectorAll('.guilds-wrapper .badge');
+    direct += getAlertCount(guildDirect);
+    const channelDirect = document.querySelectorAll('[class^="nameUnreadText-"]+div>div>div');
+    direct += getAlertCount(channelDirect);
+    
+    // get indirect messages
+    let indirect = document.querySelectorAll('.guilds-wrapper .unread').length;
+    indirect += document.querySelectorAll('[class^="nameUnreadText-"]').length;
 
     // set Franz badge
-    Franz.setBadge(count);
+    Franz.setBadge(direct, indirect);
   };
 
   // check for new messages every second and update Franz badge
   Franz.loop(getMessages);
 
   // Hide download message
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  Franz.injectCSS(_path2.default.join(__dirname, 'service.css'));
+  
+  // Parse alert count from badges
+  function getAlertCount(badges) {
+    let alerts = 0;
+    for(let i = 0; i < badges.length; i++) {
+      const badge = badges[i];
+      if(badge && badge.childNodes && badge.childNodes.length > 0) {
+        const count = parseInt(badge.childNodes[0].nodeValue, 10);
+        alerts += count.isNaN ? 0 : count;
+      }
+    }
+    return alerts;
+  };
 };
